1f2979432258d4609350fa118fa8aaefb55a6008
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-09-03 09:27:10 +0630

    layout

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router";
-import App from '../pages/Home.jsx'
+import Home from '../pages/Home.jsx'
+import Layout from "../pages/layouts/Layout.jsx";
+import Create from "../pages/Create.jsx";
+import Search from "../pages/Search.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Layout />, 
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "/create",
+        element: <Create/>
+      },
+      {
+        path: "/search",
+        element: <Search/>
+      }
+    ]
   },
 ]);
 
