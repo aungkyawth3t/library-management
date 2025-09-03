21f7b77c69031d0fcbb82e6848a0ce4d3e694055
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-09-03 10:20:19 +0630

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav>
+            <ul className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+                <li className='flex items-center gap-3'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                        <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
+                    </svg>
+                    <input type="text" placeholder='Search' className='outline-none '/>
+                </li>
+                <Link to="/" className='flex items-center gap-3 md:-ml-32 cursor-pointer'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                        <path d="M11.584 2.376a.75.75 0 0 1 .832 0l9 6a.75.75 0 1 1-.832 1.248L12 3.901 3.416 9.624a.75.75 0 0 1-.832-1.248l9-6Z" />
+                        <path fillRule="evenodd" d="M20.25 10.332v9.918H21a.75.75 0 0 1 0 1.5H3a.75.75 0 0 1 0-1.5h.75v-9.918a.75.75 0 0 1 .634-.74A49.109 49.109 0 0 1 12 9c2.59 0 5.134.202 7.616.592a.75.75 0 0 1 .634.74Zm-7.5 2.418a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Zm3-.75a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 1-1.5 0v-6.75a.75.75 0 0 1 .75-.75ZM9 12.75a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Z" clipRule="evenodd" />
+                        <path d="M12 7.875a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" />
+                    </svg>
+                    <span className='text-2xl font-bold text-indigo-600 hidden md:block'>My Library</span>
+                </Link>
+                <li className='flex gap-3 items-center'>
+                    <Link to= "/create" className='text-white bg-indigo-600 px-3 py-2 rounded-2xl cursor-pointer flex items-center gap-1'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
+                        </svg>
+                        <span className='hidden md:block'> Create Book </span>
+                    </Link>
+                    <div className='w-11'>
+                        <img className='w-full rounded-full' src="https://avatars.githubusercontent.com/u/193622492?s=48&v=4" alt="" />
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
