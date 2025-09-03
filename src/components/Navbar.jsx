f96ba2dbafb0723aaa990baf6e7da330509db273
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-09-03 16:13:43 +0630

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
                         <path fillRule="evenodd" d="M20.25 10.332v9.918H21a.75.75 0 0 1 0 1.5H3a.75.75 0 0 1 0-1.5h.75v-9.918a.75.75 0 0 1 .634-.74A49.109 49.109 0 0 1 12 9c2.59 0 5.134.202 7.616.592a.75.75 0 0 1 .634.74Zm-7.5 2.418a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Zm3-.75a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 1-1.5 0v-6.75a.75.75 0 0 1 .75-.75ZM9 12.75a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Z" clipRule="evenodd" />
                         <path d="M12 7.875a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" />
                     </svg>
-                    <span className='text-2xl font-bold text-indigo-600 hidden md:block'>My Library</span>
+                    <span className='text-2xl font-bold text-indigo-600 hidden md:block'>Shelfio</span>
                 </Link>
                 <li className='flex gap-3 items-center'>
                     <Link to= "/create" className='text-white bg-indigo-600 px-3 py-2 rounded-2xl cursor-pointer flex items-center gap-1'>
