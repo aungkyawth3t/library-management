d978c0899e22c01ec32388827bb4b1a628373420
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-10-22 22:51:29 +0630

    pull

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,30 +1,12 @@
-import { createContext, useReducer } from "react";
+import { createContext } from "react";
 
 let ThemeContext = createContext();
 
-let ThemeReducer = (state, action) => {
-    switch(action.type) {
-        case "CHANGE_THEME" : 
-            return {...state, theme: action.payload}
-        default : 
-            return state;
-    }
-}
-
 // theme context provider
+
 const ThemeContextProvider = ({ children }) => {
-
-    let [state, dispatch] = useReducer(ThemeReducer, {
-        theme: 'light'
-    });
-
-    let changeTheme = () => {
-        // action = type + payload -> {type, payload}
-        dispatch({type : 'CHANGE_THEME', payload : 'dark'});
-    }
-
     return (
-        <ThemeContext.Provider value={{...state, changeTheme}}>
+        <ThemeContext.Provider value={{theme: "dark"}}>
             {children}
         </ThemeContext.Provider>
     )
